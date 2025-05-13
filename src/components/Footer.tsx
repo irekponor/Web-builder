b4956e9a10fd82a4c7f376353f838ea5fd4985b6
emmanuel <emmanuelirekponor86@gmail.com>
2025-05-14 00:49:19 +0100

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,10 @@ import { FaHeart } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div>
-      <h1>
-        Made with <FaHeart /> by Emmanuel irekponor
+    <div className="pl-90 ml-43">
+      <h1 className="flex">
+        <i className="pr-2">Made with</i> <FaHeart className="text-red-700" />
+        <i className="pl-2">by Emmanuel irekponor</i>
       </h1>
     </div>
   );
