bda45a85f6dc4f6d5d62d5a7e4319036c3a7562e
emmanuel <emmanuelirekponor86@gmail.com>
2025-05-16 00:28:27 +0100

    edit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { FaHeart } from "react-icons/fa6";
 
 const Footer = () => {
   return (
-    <div className="md:pl-90 md:ml-24 max-md:mt-[49rem] ">
+    <div className="md:pl-90 md:ml-44 max-md:mt-[49rem] ">
       <h1 className="flex max-md:pl-12">
         <i className="pr-2">Made with</i> <FaHeart className="text-red-700" />
         <i className="pl-2">by Emmanuel Irekponor</i>
